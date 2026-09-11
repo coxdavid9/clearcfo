ca8ed73d01a2db86618e77c3962396c4e44629cc
coxdavid9 <tbwnzvp2kr@privaterelay.appleid.com>
2026-09-11 07:40:09 -0500

    Add logout route

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import { getAuthCookieNames } from "../../../../lib/supabase-auth";
+
+export const runtime = "nodejs";
+
+export async function GET(request: Request) {
+  const response = NextResponse.redirect(new URL("/", request.url));
+  const { AUTH_COOKIE, REFRESH_COOKIE } = getAuthCookieNames();
+
+  response.cookies.set(AUTH_COOKIE, "", { httpOnly: true, expires: new Date(0), path: "/" });
+  response.cookies.set(REFRESH_COOKIE, "", { httpOnly: true, expires: new Date(0), path: "/" });
+
+  return response;
+}
