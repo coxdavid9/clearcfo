6827f5544df77879ff4810ff8a52a35c460909b1
coxdavid9 <tbwnzvp2kr@privaterelay.appleid.com>
2026-09-15 07:09:18 -0500

    Add signup link to homepage navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <main id="top" className="min-h-screen bg-white text-slate-900">
-      <Navbar loginHref="/login" loginLabel="Log In" sessionAware />
+      <Navbar loginHref="/login" loginLabel="Log In" signupHref="/login?mode=signup" signupLabel="Sign Up" sessionAware />
       <section id="product" className="scroll-mt-24 bg-gradient-to-b from-blue-50/70 via-white to-white"><Hero /></section>
       <section id="how-it-works" className="scroll-mt-24 border-y border-slate-200/70 bg-white"><HowItWorks /></section>
       <section id="what-you-receive" className="scroll-mt-24 bg-slate-50/70"><Features /></section>
